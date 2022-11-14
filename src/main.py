03340711552875f7af74e0bbdf87436bc9be4688
Muhammad Hanif Amrullah Fakhruddin <hamexhanif@yahoo.com>
2022-11-14 19:36:44 +0100

    convert video to grayscale and add comments

--- src/main.py
+++ src/main.py
@@ -5,9 +5,9 @@ cap = cv2.VideoCapture(0)
 
 while True:
     ret, frame = cap.read()
-    cv2.imshow('frame', frame)
+    cv2.imshow('frame', cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)) #convert video to grayscale
 
-    if cv2.waitKey (1) & 0xFF == ord('q'):
+    if cv2.waitKey (1) & 0xFF == ord('q'): # press q to end video capture
         break
 
 cap.release()
